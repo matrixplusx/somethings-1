d7fe27d3c39e5d6cf7af5971e6dc51c375d8adac
matrixplusx <33364116+matrixplusx@users.noreply.github.com>
2018-09-11 17:04:05 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -6,7 +6,7 @@ local_ip=`ss|grep ssh|awk 'NR==1{print $4}'|cut -d \: -f1`
 find . -name "*.gz"|xargs -i tar -xf {}
 find . -name "config.xml"|xargs -i sed -i "s/192.168.126.132/${local_ip}/g" {}
 find . -name "config.xml"|xargs -i sed -i "s/cdn_db/${version}/g" {}
-find . -name "config.xml"|xargs -i sed -i "s#suma</db_passwd#123456</${db_passwd}#g" {}
+find . -name "config.xml"|xargs -i sed -i "s#suma</db_passwd#${db_passwd}</db_passwd#g" {}
 find . -name "*.sql"|grep mysql|xargs -i sed -i "s/cdn_db/${version}/g" {}
 mysql -uroot -p"${db_passwd}" -e "drop database ${version};"
 for i in `find . -name "*.sql"|grep mysql`;do mysql -uroot -p"${db_passwd}" -e "source ${i};";done
